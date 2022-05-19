a20cb80738500d16e5d5dc9140e7237ef1e22692
Abai <abai.org@gmail.com>
2022-05-19 16:12:41 +0600

    account app: Adding 2 models - UserRate, UserFavorite (bookmark)

--- account/models.py
+++ account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+
+class UserRate(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='rates')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    rate = models.IntegerField(default=0, validators=[MinValueValidator(-1), MaxValueValidator(1)])
+
+
+class UserFavorite(models.Model):   # Пользователь возможно будет добавлять в избранное и другое, н-р: изобр., коллекц..
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='favorites')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
